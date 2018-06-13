b186bd2f242ad65de880dc03b5329fae5990c7a4
Brad Gibbons <bradgibbonsdev@gmail.com>
2018-06-13 11:23:34 -0400

    updated working game with 12 teams

--- script.js
+++ script.js
@@ -35,10 +35,6 @@ const cardsArray = [{
     'img': 'images/new-york-knicks-logo-vector.png',
   },
   {
-    'name': 'Magic',
-    'img': 'images/orlando-magic-logo-vector.png',
-  },
-  {
     'name': '76ers',
     'img': 'images/philadelphia-76ers-logo-vector.png',
   },
@@ -52,9 +48,15 @@ const cardsArray = [{
   }
 ];
 let gameGrid = cardsArray.concat(cardsArray);
+gameGrid.sort(() => 0.5 - Math.random());
 
+//init count & guesses
+let delay = 1200
+let previousTarget = null;
+let count = 0;
+let firstGuess = '';
+let secondGuess = '';
 //grab div
-
 const game = document.getElementById('game');
 
 //create section
@@ -71,8 +73,71 @@ gameGrid.forEach(item => {
     card.classList.add('card');
     // set data name to key name
     card.dataset.name = item.name;
+    // create front of card
+    const front = document.createElement('div');
+    front.classList.add('front');
+
+    // Create back of card
+    const back = document.createElement('div');
+    back.classList.add('back');
     // apply background image
-    card.style.backgroundImage = `url(${item.img})`;
+    back.style.backgroundImage = `url(${item.img})`;
     // append to section
     grid.appendChild(card);
+    card.appendChild(front);
+    card.appendChild(back);
 });
+
+// Event listeners
+grid.addEventListener('click', function (event) {
+    // The event target is our clicked item
+    let clicked = event.target;
+  
+    // Do not allow the grid section itself to be selected; only select divs inside the grid
+    if (clicked.nodeName === 'SECTION' || clicked === previousTarget) { return; }
+    
+    if(count < 2) {
+        count ++;
+        if(count === 1) {
+            // assign to first
+            firstGuess = clicked.parentNode.dataset.name;
+            console.log(firstGuess);
+            clicked.parentNode.classList.add('selected');
+        } else {
+            secondGuess = clicked.parentNode.dataset.name;
+            console.log(secondGuess);
+            clicked.parentNode.classList.add('selected');
+        }
+    }
+    // if both guesses are not empty
+    if(firstGuess !== '' && secondGuess !== '') {
+        if(firstGuess === secondGuess) {
+            // its a match
+            setTimeout(match, delay);
+            setTimeout(resetGuesses, delay);
+        } else {
+            setTimeout(resetGuesses, delay);
+        }
+    }
+    previousTarget = clicked;
+   });
+
+   // Is it a match?
+   const match = () => {
+    var selected = document.querySelectorAll('.selected');
+    selected.forEach(card => {
+      card.classList.add('match');
+    });
+  };
+
+  // Not a match
+  const resetGuesses = () => {
+    firstGuess = '';
+    secondGuess = '';
+    count = 0;
+  
+    var selected = document.querySelectorAll('.selected');
+    selected.forEach(card => {
+      card.classList.remove('selected');
+    });
+  };
